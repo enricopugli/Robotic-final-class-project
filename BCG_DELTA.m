6330c88c56317fe3afbfbe632d242a4d361176a8
Enrico Puglisi <enrico.puglisi@gmail.com>
2018-12-07 21:40:03 +0100

    Minor

--- BCG_DELTA.m
+++ BCG_DELTA.m
@@ -2,7 +2,7 @@ function [B, C, G, F] = BCG_DELTA(ee, q)
 %UNTITLED6 Summary of this function goes here
 %   Detailed explanation goes here
 
-[theta, ~, ~, m_br, ~, r_Gb, ~, ~, ~, m_nt, I_bi] = Parameters_DELTA;
+[~, ~, ~, m_br, ~, r_Gb, ~, ~, ~, m_nt, I_bi] = Parameters_DELTA;
 
 % Arms intertia matrix
 I_b = diag([I_bi, I_bi, I_bi]);
@@ -18,7 +18,7 @@ C = (J'*m_nt*dJ);
 
 % Gravitational elements matrix
 G_n = m_nt*[0; 0; -9.81];
-G_b = m_br*[0; 0; -9.81];
+% G_b = m_br*[0; 0; -9.81];
 
 % tau_b =  r_Gb*[ cos(theta(1)); cos(theta(2)); -m_br*9.8*cos(theta(3))];
 
--- PtP_TrajGeneration.m
+++ PtP_TrajGeneration.m
@@ -1,5 +1,5 @@
 p_in = [0.12; 0.12; -.3];
-p_end = [0; 0; -.5];
+p_end = [0; 0; -.4];
 
 v_in = zeros(3,1);
 v_end = zeros(3,1);
@@ -28,4 +28,4 @@ dq_param_traj = [3*param_traj(1,:); 2*param_traj(2,:); param_traj(3,:)];
 ddq_param_traj = [6*param_traj(1,:); 2*param_traj(2,:)];
     
 filename = 'param_traj';
-save(filename, 'param_traj');
+save(filename, 'param_traj', 'dq_param_traj', 'ddq_param_traj');
