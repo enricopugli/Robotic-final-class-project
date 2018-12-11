d7ec5c0d129909899b2392b355d2403fe92d2904
AndreaDan <Andrea.d.ryals@gmail.com>
2018-12-11 13:08:28 +0100

    calcolo del regressore

---
+++ Regressor_Symbolic_Computation.m
@@ -0,0 +1,91 @@
+%%% ------------------------------------------------------------------- %%%
+%%% ---------- Symbolic Regressor Computation ------------------------- %%%
+%%% ------------------------------------------------------------------- %%%
+
+syms pi_1 pi_2 pi_3 pi_4 pi_5 pi_6 
+
+pi_1  = m_b;
+pi_2  = m_c;
+pi_3  = m_fb*r_m;
+pi_4  = m_n;
+pi_5  = m_p;
+pi_6  = F;
+
+
+pi_vec = [pi_1 pi_2 pi_3 pi_4 pi_5 pi_6].';
+% length(q_vec)
+
+% Regressor columns computation      
+Y = sym('Y', [3,length(pi_vec)]);
+
+for i = 1:3
+    
+    %---------------------------------------------------------------------   
+    % take every term with coefficients formed by m1, r1 (see t1)
+    [c1,t1] = coeffs(Dyn(i), m_b);
+    C1 = arrayfun(@char,t1,'Un',0);
+    % take only term with coefficients formed by m1, r1 and characters
+    % * ('\w*\*\w*'), thus selecting only terms with m1*r1^2
+    % (see regexp(C1,'\w*\*\w*','match'))
+    C1 = c1(~cellfun('isempty',regexp(C1,'\w*\*\w*','match')));
+
+    if isempty(C1)
+        C1 = 0;
+    end
+    Y(i,1) = C1;
+
+    %---------------------------------------------------------------------
+    [c2,t2] = coeffs(Dyn(i), m_c);
+    C2 = arrayfun(@char,t2,'Un',0);
+    C2 = c2(~cellfun('isempty',regexp(C2,'\w*\*\w*','match')));
+
+    if isempty(C2)
+        C2 = 0;
+    end
+    Y(i,2) = C2;
+
+    %---------------------------------------------------------------------
+    [c3,t3] = coeffs(Dyn(i), [m_fb,r_m]);
+    C3 = arrayfun(@char,t3,'Un',0);
+    C3 = c3(~cellfun('isempty',regexp(C3,'\w*\*\w*','match')));
+
+    if isempty(C3)
+        C3 = 0;
+    end
+    Y(i,3) = C3;
+
+    %---------------------------------------------------------------------
+    [c4,t4] = coeffs(Dyn(i), m_n);
+    C4 = arrayfun(@char,t4,'Un',0);
+    C4 = c4(~cellfun('isempty',regexp(C4,'\w*\*\w*','match')));
+
+    if isempty(C4)
+        C4 = 0;
+    end
+    Y(i,4) = C4;
+
+    %---------------------------------------------------------------------
+    [c5,t5] = coeffs(Dyn(i), m_p);
+    C5 = arrayfun(@char,t5,'Un',0);
+    C5 = c5(~cellfun('isempty',regexp(C5,'\w*\^\w*\*\w*','match')));
+
+    if isempty(C5)
+        C5 = 0;
+    end
+    Y(i,5) = C5;
+
+    %---------------------------------------------------------------------
+    [c6,t6] = coeffs(Dyn(i), F);
+    C6 = arrayfun(@char,t6,'Un',0);
+    C6 = c6(~cellfun('isempty',regexp(C6,'\w*\^\w*\*\w*','match')));
+
+    if isempty(C6)
+        C6 = 0;
+    end
+    Y(i,6) = C6;
+
+end
+
+clear C1 C2 C3 C4 C5 C6 
+clear c1 c2 c3 c4 c5 c6 
+clear t1 t2 t3 t4 t5 t6 
--- SymDyn_DELTA.m
+++ SymDyn_DELTA.m
@@ -1,13 +1,51 @@
 % Symbolic variables
 
 q = sym('q', [3 1]);
+dq = sym('dq', [3 1]);
+ddq = sym('ddq', [3 1]);
 ee = sym('ee', [3 1]);
 
 % Load of parameters and symbolic parameters selection
-[theta, R, m_b, m_br, l_A, r_Gb, m_fb, l_B, m_n, m_nt, I_bi]  = Parameters_DELTA;
+% [theta, R, m_b, m_br, l_A, r_Gb, m_fb, l_B, m_n, m_nt, I_bi]  = Parameters_DELTA;
 
-% I_bc = l_A^2*(m_b/3 + m_c + r_m*m_fb); % [Kg*m^2] Inertia of the arm
-% I_bi = (I_m + I_brake)*k_r^2 + I_bc; % [Kg*m^2] Total inertia contribution of each upper arm
+% Adimensional parameters for a regular shape of the working volume. From
+% [2] pg. 405
+% r_l = R/lA
+r_l = 0.63; % VERIFICA
+% b = l_B/l_A
+b = 2; 
+
+% Arms parameters
+syms m_b; % [kg] Upper arms mass
+syms m_c; % [Kg] Elbow mass
+syms m_fb; % [Kg] Forearms mass
+syms r_m; % Ratio of forearm mass at upper extremis
+
+m_br = m_b + m_c + r_m*m_fb; 
+
+l_A = 0.2; % [m] Upper arms length
+l_B = b*l_A; % [m] Forearms length
+r_Gb = l_A*(0.5*m_b + m_c + r_m*m_fb)/m_br; % center of the mass of upper arms
+
+% Base plate parameters
+R = r_l*l_A;
+theta = [0, 2*pi/3, 4*pi/3];
+
+% Payload parameters
+syms m_p; % [Kg] payload mass
+
+% Travelling plate parameters
+syms m_n; % [Kg] 
+m_nt = m_n + m_p + 3*(1-r_m*m_fb); % [Kg] total mass contribute of the travelling plate
+
+% Motors parameters
+k_r = 1; % Gear reduction ratio
+
+% Inertia 
+I_m = 0; % [Kg*m^2] Inertia of the motor
+I_brake = 0; % [Kg*m^2] Inertia of the motor
+I_bc = l_A^2*(m_b/3 + m_c + r_m*m_fb); % [Kg*m^2] Inertia of the arm
+I_bi = (I_m + I_brake)*k_r^2 + I_bc; % [Kg*m^2] Total inertia contribution of each upper arm
 % 
 I_b = diag([I_bi, I_bi, I_bi]);
 
@@ -27,7 +65,9 @@ G_n = m_nt*[0; 0; -9.81];
 tau_Gb = r_Gb*9.81*m_br*[ cos(q(1)); cos(q(2)); cos(q(3))];
 
 G = (J'*G_n + tau_Gb);
-F = 0.6;
+syms F;
+
+Dyn = B*ddq + C*dq + F*dq + G;
 
 function [J,dJ] = Jacobian_DELTA(ee, q)
 % Give back Jacobian matrix and its derivative
