95c463f2cd9affb41e4bb2e0e211ce362a0bffb2
Enrico Puglisi <enrico.puglisi@gmail.com>
2019-01-01 22:55:20 +0100

    fine pomeriggio

--- DELTA/excitability_script.m
+++ DELTA/excitability_script.m
@@ -1,9 +1,20 @@
-eig_excit = nan(1,length(excitability));
+close all
+clear eig_excit;
+% eig_excit = nan(3,length(excitability));
 
-for i = 1:length(excitability)
-    eig_excit(i) = eig(excitability(:,:,i));
+j = 1;
+for i = 1:100:length(excitability)
+    eig_excit(:,j) = eig(excitability(:,:,i));
+    rank_excit(:,j) = rank(excitability(:,:,i));
+    j = j +1;
 end    
 
 figure
-plot(eig_excit(1), eig_excit(2), eig_excit(3));
+plot(1:length(eig_excit(1,:)),eig_excit(1,:),...
+     1:length(eig_excit(1,:)),eig_excit(2,:),...
+     1:length(eig_excit(1,:)),eig_excit(3,:));
+grid on
+
+figure
+plot(1:length(rank_excit), rank_excit);
 grid on
--- DELTA/untitled2.m
+++
@@ -1,9 +0,0 @@
-eig_excit = nan(3,length(excitability));
-
-for i = 1:length(excitability)
-    eig_excit(:,i) = eig(excitability(:,:,i));
-end    
-
-figure
-plot(eig_excit(2,:));
-grid on
