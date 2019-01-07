2e72c82f32eb93f75d8dbd9f0e4e96efe5004c1b
AndreaDan <Andrea.d.ryals@gmail.com>
2019-01-07 10:14:09 +0100

    0

--- DELTA/untitled2.m
+++ DELTA/untitled2.m
@@ -5,5 +5,11 @@ for i = 1:length(excitability)
 end    
 
 figure
-plot(eig_excit(2,:));
+plot(eig_excit(1:3,:));
+plot(eig_excit(1:3,1:end));
+grid on
+
+
+figure
+plot(eig_excit(1:3,1:end));
 grid on
