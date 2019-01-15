50567d307ead0cd2b755264a126db15838e18858
Enrico Puglisi <enrico.puglisi@gmail.com>
2019-01-15 01:06:44 +0100

    slides

--- BALL_PLATE/Observability_BP.m
+++ BALL_PLATE/Observability_BP.m
@@ -14,10 +14,17 @@ g2 = [0; 0; 0; 0; 0; 0; 0; 1];
 
 h1 = [x(1), zeros(1,7)];
 h2 = [0, x(2), zeros(1,6)];
+h3 = [0, 0, x(3), zeros(1,5)];
+h4 = [0, 0, 0, x(4), zeros(1,4)];
 
 %%
 ad_fnh1 = liebracket(f,h1,x,7);
 
-%%
 ad_fnh2 = liebracket(f,h2,x,7);
 
+ad_fnh3 = liebracket(f,h3,x,7);
+
+ad_fnh4 = liebracket(f,h4,x,7);
+
+%%
+O = [ad_fnh1, ad_fnh2, ad_fnh3, ad_fnh4];
