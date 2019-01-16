535ba3a03a2c2e6ca811f4caa7a5a1ebdd5f38fd
Enrico Puglisi <enrico.puglisi@gmail.com>
2019-01-16 13:54:52 +0100

    slides

--- BALL_PLATE/Controllability_BP.m
+++ BALL_PLATE/Controllability_BP.m
@@ -28,6 +28,7 @@ f = [x(5:8);
 
 g1 = [0; 0; 0; 0; 0; 0; 1; 0];
 g2 = [0; 0; 0; 0; 0; 0; 0; 1];
+load('E_diagonal_form_of_Q.mat'); % matrice E dopo inversione delle righe
 
 %%
 ad_fng1 = liebracket(f,g1,x,7);
@@ -37,9 +38,14 @@ ad_fng2 = liebracket(f,g2,x,7);
 
 %%
 Q = [ad_fng1, ad_fng2];
-
+load('E_diagonal_form_of_Q.mat'); % matrice E dopo inversione delle righe
 %% Columns and row swapping for original model
 % Q_swap = [Q(:,7),Q(:,2:6),Q(:,1),Q(:,8:16)];
 % Q_swap = [Q_swap(:,1:7),Q_swap(:,9),Q_swap(:,8),Q_swap(:,10:16)];
 % Q_swap = [Q_swap(:,1:3),Q_swap(:,10),Q_swap(:,5:9),Q_swap(:,4), Q_swap(:,11:16)];
 % Q_swap = [Q_swap(:,3),Q_swap(:,1:2),Q_swap(:,4:16)];
+
+%% Analisi della matrice E
+
+E_det_el = diag(E);
+
